80d1c7cc51bf091364cc7fa646dd6515bcd6d400
divinejoshua <divineuche399@gmail.com>
2023-05-24 23:41:52 +0100

    moved javascript to sketch js

---
+++ sketch.js
@@ -0,0 +1,45 @@
+    // Your code will go here
+    // open up your console - if everything loaded properly you should see the latest ml5 version
+    console.clear()
+
+    let video = null
+    let poses = []
+    let noseX = null
+    let noseY = null
+
+    // On set up 
+    function setup() {
+        createCanvas(600,500);
+        video = createCapture(VIDEO)
+        video.size(600, 500)
+
+        let poseFinder = ml5.poseNet(video); 
+        // Listen to new 'pose' events
+        poseFinder.on('pose', (results) => {
+            poses = results;
+          });
+                    
+        video.hide()
+    }
+
+    // On camera 
+    function draw() {
+        background(220);
+        image(video,0,0,600,500)
+        redDotAnimation() //Red dot animation
+    }
+
+
+
+    //Animations 
+    function redDotAnimation(){
+        //This animation is a red dot on the nose
+        if(poses.length > 0) {
+            noseX = poses[0].pose.nose.x
+            noseY = poses[0].pose.nose.y
+            
+            fill(255,0,0)
+            ellipse(noseX,noseY, 50)  
+        }
+
+    }
