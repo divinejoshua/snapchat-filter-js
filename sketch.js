14d38407d1504528368f6931b3cfcdeafcdfb875
divinejoshua <divineuche399@gmail.com>
2023-05-25 00:26:54 +0100

    Completed full clown filter

--- sketch.js
+++ sketch.js
@@ -9,7 +9,11 @@
 
     // Create a new image element
     var redDotImg = new Image();
-    redDotImg.src = 'images/reddot.png'; // Replace with the path to your image
+    redDotImg.src = 'images/reddot.png';
+
+     // Create a new image element
+    var clownHatImg = new Image();
+    clownHatImg.src = 'images/clown-hat.png';
 
 
     // On set up 
@@ -32,21 +36,22 @@
         background(220);
         image(video,0,0,600,500)
         redDotAnimation() //Red dot animation
+        clownHatAnimation() //Red dot animation
         
     }
 
 
 
-    //Animations 
+    //===================Animations ====================
+
+    //This animation is a red dot on the nose
     function redDotAnimation(){
-        //This animation is a red dot on the nose
         if(poses.length > 0) {
-            console.log(poses[0].pose)
             noseX = poses[0].pose.nose.x
             noseY = poses[0].pose.nose.y
             redDotImg.style.position = 'absolute';
             redDotImg.style.left = noseX-17 + 'px';
-            redDotImg.style.top = noseY + 'px';
+            redDotImg.style.top = noseY + 50 + 'px';
             redDotImg.style.width = 50 + 'px';
             redDotImg.style.height = 50 + 'px';
 
@@ -55,6 +60,22 @@
              
         }
 
-      
+    }
+
+    //This animation is for the clown hat
+    function clownHatAnimation(){
+        if(poses.length > 0) {
+            noseX = poses[0].pose.nose.x
+            noseY = poses[0].pose.nose.y
+            clownHatImg.style.position = 'absolute';
+            clownHatImg.style.left = noseX-200 + 'px';
+            clownHatImg.style.top = noseY - 350 + 'px';
+            clownHatImg.style.width = 400 + 'px';
+            clownHatImg.style.height = 400 + 'px';
+
+            // Add the red dot to the dom 
+            document.body.appendChild(clownHatImg);
+             
+        }
 
     }
